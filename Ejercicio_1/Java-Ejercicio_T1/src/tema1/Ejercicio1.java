8e4f2d56f55724194760d8c5ac9c7b68d88efe5f
alberto020596 <50453193+alberto020596@users.noreply.github.com>
2023-02-01 22:02:52 -0600

    Ejercicio 1 Java Basico

---
+++ Ejercicio_1/Java-Ejercicio_T1/src/tema1/Ejercicio1.java
@@ -0,0 +1,31 @@
+package tema1;
+
+public class Ejercicio1 {
+
+    public static void main (String[] arg){
+
+        //tipos datos
+
+        //enteros
+        byte dato1= 1;
+        short dato2=12;
+        int  dato3= 234;
+        long datos4=10004;
+
+        //decimales
+
+        float decimal1=3.14f;
+        double decimal2=456.789d;
+
+        //texto
+
+        char letra='a';
+        String cadena= "Hola";
+
+        boolean variable = true;
+        Integer variable2= null;
+
+
+    }
+
+}
